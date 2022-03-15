5853bb971dd497377a3e7a46a407e7bb4548f84b
DegOlgaVik <ov.degteva@gmail.com>
2022-03-15 18:03:38 +0300

    Add test DZ#7

---
+++ src/main/java/TestPages.java
@@ -0,0 +1,15 @@
+import pages.MainPage;
+import pages.ProfilePage;
+import pages.ReleasePage;
+import pages.RepoPage;
+
+public final class TestPages {
+
+    public static final MainPage mainPage = new MainPage();
+    public static final ProfilePage profilePage = new ProfilePage();
+    public static final RepoPage repoPage = new RepoPage();
+    public static final ReleasePage releasePage = new ReleasePage();
+
+    private TestPages() {
+    }
+}
