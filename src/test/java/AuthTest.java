5e34cf1e9c63489153620251cc549e300828e405
DegOlgaVik <ov.degteva@gmail.com>
2022-03-17 01:05:52 +0300

    Add test DZ#8

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +12,11 @@ import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@Owner("DegOlgaVik")
+@Feature("Проверка авторизации")
 
 public class AuthTest {
 
@@ -23,43 +30,60 @@ public class AuthTest {
     }
 
     @Test
+    @Story("Авторизация")
     @DisplayName("Успешная авторизация")
     public void shouldAuthorizeTest() {
+        step("Заполнить поля инпута и пароля и нажать кнопку авторизации", () -> {
         //3. Заполнить инпуты логина и пароля
         TestPages.mainPage.loginInput()
-                .sendKeys("D**O***V**");
+                .sendKeys("DegOlgaVik");
         TestPages.mainPage.passwordInput()
-                .sendKeys("*1**11**1***");
+                .sendKeys("*1**1***1***");
         //4. Нажать кнопку sign in
         TestPages.mainPage.signInButton()
                 .click();
+        });
+
+        step("Проверка авторизации", () -> {
         //5. Проверить авторизацию
         TestPages.mainPage.header()
                 .shouldBe(visible);
+        });
+
+        step("Раскрытие информации и переход на страницу профайла", () -> {
         //6. Нажать на значок аватарки, чтобы он раскрылся
         TestPages.profilePage.headerlink()
                 .click();
         //7. Выбрать "Your profile"
         TestPages.profilePage.dropdownItem()
                 .click();
+        });
+
+        step("Проверка переходна на страницу Your profile", () -> {
         //8. Проверить переход на страницу профайла
         TestPages.profilePage.avatarGroupItem()
                 .shouldBe(visible);
+        });
     }
 
     @MethodSource("incorrectCredentials")
     @ParameterizedTest(name = "{displayName} {0}")
     @DisplayName("Авторизация с некорректными данными:")
     public void shouldNotAuthorizeTest(String type, String phone, String password){
+        step("Заполнить поля инпута и пароля и нажать кнопку авторизации", () -> {
         TestPages.mainPage.loginInput()
                 .sendKeys(phone);
         TestPages.mainPage.passwordInput()
                 .sendKeys(password);
         TestPages.mainPage.signInButton()
                 .click();
-        TestPages.mainPage.incorrectInfo()
-                .shouldBe(visible);
-    }
+        });
+
+        step("Проверить, что появилась ошибка", () -> {
+            TestPages.mainPage.incorrectInfo()
+                    .shouldBe(visible);
+        });
+        }
 
     static Stream<Arguments> incorrectCredentials() {
         return Stream.of(
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,7 +12,11 @@ import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@Owner("DegOlgaVik")
+@Feature("Поиск по репозиторию")
 
 public class SearchTest {
 
@@ -20,29 +27,42 @@ public class SearchTest {
     }
 
     @Test
+    @Story("Переход на ветку fixtures")
     @DisplayName("Переключение на ветку fixtures")
     public void setup1() {
+        step("Выбор ветки fixtures", () -> {
         //2. Кликнуть на кнопку id="branch-select-menu"
         TestPages.repoPage.branchSelectButton()
                 .click();
         //3. Выбрать ветку fixtures
         TestPages.repoPage.fixtures()
                 .click();
+        });
+
+        step("Проверка перехода на ветку fixtures", () -> {
         //4. Проверить переход на страницу fixtures ветки
         TestPages.repoPage.fixturesBranch()
                 .shouldBe(visible);
+        });
     }
 
     @MethodSource("incorrectCredentials")
     @ParameterizedTest(name = "{displayName} {0}")
+    @Story("Поиск по номерам и буквам релизов")
     @DisplayName("Позитивные проверки поиска по релизам:")
     public void positiveSearchChecksTest(String type, String name){
+        step("Переход на страницу тегов", () -> {
         TestPages.releasePage.tagsButton()
                 .click();
+        });
+        step("Переход на страницу релизов", () -> {
         TestPages.releasePage.releaseButton()
                 .click();
+        });
+        step("Поиск по номеру и буквам в названии", () -> {
         TestPages.releasePage.searchButton()
                 .sendKeys(name);
+        });
     }
 
     static Stream<Arguments> incorrectCredentials() {
